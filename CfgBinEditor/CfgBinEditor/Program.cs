82d10d437505f4b93c89b549b318e0ba925e68c2
onepiecefreak3 <realonepiecefreak@gmx.de>
2026-04-26 12:13:22 +0200

    Add more fonts;

--- CfgBinEditor/CfgBinEditor/Program.cs
+++ CfgBinEditor/CfgBinEditor/Program.cs
@@ -6,7 +6,6 @@ using CrossCutting.Core.Contract.EventBrokerage;
 using CrossCutting.Core.Contract.Messages;
 using ImGui.Forms;
 using ImGui.Forms.Localization;
-using ImGui.Forms.Models;
 using ImGui.Forms.Factories;
 
 KernelLoader loader = new();
@@ -18,11 +17,14 @@ eventBroker.Raise(new InitializeApplicationMessage());
 var localizer = kernel.Get<ILocalizer>();
 var app = new Application(localizer);
 
+FontFactory.RegisterFromResource("Roboto", "roboto.ttf");
 FontFactory.RegisterFromResource("NotoJp", "notojp.ttf");
+FontFactory.RegisterFromResource("NotoKr", "notokr.ttf");
+FontFactory.RegisterFromResource("NotoZh", "notozhtc.ttf");
 
 var formFactory = kernel.Get<IFormFactory>();
 
 MainForm form = formFactory.CreateMainForm();
-form.DefaultFont = FontFactory.GetDefault(13, FontFactory.Get("NotoJp", 15));
+form.DefaultFont = FontFactory.GetDefault(13, FontFactory.Get("Roboto", 13, FontFactory.Get("NotoJp", 15, FontFactory.Get("NotoKr", 15, FontFactory.Get("NotoZh", 15)))));
 
 app.Execute(form);
